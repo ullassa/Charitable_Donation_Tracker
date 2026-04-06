8d7f001c0109c66529f816e125c5505380f57400
ShreyaUdupi05 <shreyakunder2@gmail.com>
2026-04-06 17:09:21 +0530

    deleted 2 files

---
+++ Charity/Models/Customer.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Charity.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+    //hellorllllll
+}
